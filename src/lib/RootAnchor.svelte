1dce1f2cf8e53dad8b44a873373b4136c8797259
heyjoeway <joe@jojudge.com>
2025-02-16 12:07:32 -0500

    Add RootAnchor to replace previous behavior of Overlay

---
+++ src/lib/RootAnchor.svelte
@@ -0,0 +1,27 @@
+<!-- https://github.com/sveltejs/svelte/issues/1872#issuecomment-967103728 -->
+
+<script lang="ts">
+    import { onMount } from 'svelte';
+    
+    let anchor: Node;
+    
+    onMount(() => {
+        let container = document.getElementById('anchor-container');
+        if (!container) {
+            container = document.body.appendChild(document.createElement('div'));
+            container.id = 'anchor-container';
+        }
+        container.appendChild(anchor)
+                
+        // On Unmount
+        return () => {
+            container.removeChild(anchor);
+        }
+    });
+    
+    
+</script>
+
+<div bind:this={anchor} class="anchor">
+    <slot />
+</div>
